f6149269828923936801832cc0bd9128879b84f7
andela-mabdussalam <mariam.abdussalam@andela.com>
2017-10-16 15:29:08 +0100

    add styling for button

--- TaskList.js
+++ TaskList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, View, ListView } from 'react-native';
+import { StyleSheet, View, ListView, TouchableHighlight, Text } from 'react-native';
 import TaskRow from './TaskRow';
 
 const styles = StyleSheet.create({
@@ -10,6 +10,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
   },
+  button: {
+    height: 60,
+    borderColor: '#05A5D1',
+    borderWidth: 2,
+    backgroundColor: '#333',
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FAFAFA',
+    fontSize: 20,
+    fontWeight: '600'
+
+  }
 });
 class TaskList extends React.Component {
   constructor(props, context) {
@@ -38,6 +53,13 @@ class TaskList extends React.Component {
        key={this.props.todos}
        renderRow={this.renderRow}
       />
+      <TouchableHighlight
+        onPress={this.props.onAddStarted}
+        style={styles.button}>
+        <Text style={styles.buttonText}>
+          Add one
+        </Text>
+      </TouchableHighlight>
     </View>
     );
   }
@@ -45,5 +67,6 @@ class TaskList extends React.Component {
 
 TaskList.propTypes = {
   todos: PropTypes.arrayOf(React.PropTypes.object).isRequired,
+  onAddStarted: PropTypes.func.isRequired,
 };
 export default TaskList;
